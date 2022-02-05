35098a33f8a42f86554bd9c262f63408ebada10d
yessygc <yessygc@users.noreply.github.com>
2022-02-05 01:31:58 +0000

    deploy: 0c35ee5247d69a98d3f77a2f364f2debe6ffe303

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,36 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+// crear 1 imagen
+// agregar #imagen
+
+const minimum = 1;
+const maximum = 122;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+    const container = document.createElement("div")
+    container.className = "p-4";
+
+    const imagen = document.createElement('img')
+    imagen.className = "mx-auto";
+    imagen.width = '320'
+    imagen.src = `https://source.unsplash.com/collection/${random()}881002`; //TODO
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("button");
+
+//
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+};
+
+addButton.addEventListener("click", addImage);
